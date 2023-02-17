c7e4940b723743a161929a11f20ab853956f3b8c
Lauma Gailīte <123964250+lapsac@users.noreply.github.com>
2023-02-17 18:45:07 +0200

    main2.py
    should work

--- main.py
+++ main.py
@@ -13,22 +13,26 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(Bracket(next,i+1))
+            #pass
         if next in ")]}":
+            if not opening_bracket_stack:
+            return i+1
+        last = opening_bracket_stack.pop()
+        if not are_matching(last.char, next):
+            return i+1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    return "Success"
             # Process closing bracket, write your code here
-            pass
+            #pass
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-    if mismatch is None:
-        print(" ")
-    else:
-        print(mismatch + 1)
+    print(mismatch + 1)
 
 if __name__ == "__main__":
     main()
